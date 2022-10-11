a6e374e3b689f594768b38002990ad2959ce017f
chbr7208 <chbr7208@colorado.edu>
2022-10-11 14:01:06 -0600

    added js and used past css for ICA8, two interactive elements a checkbox and a button

---
+++ js/ica8.js
@@ -0,0 +1,17 @@
+//let body = document.querySelector('body');
+//body.addEventListener('click',giveAlert);
+
+//function giveAlert (){
+//    alert('this is an alert');
+
+
+const button = document.querySelector('#button');
+
+button.onclick = () => {
+  alert('hi this is ica8');
+}
+
+function simulateClick() {
+    const cb = document.getElementById("checkbox");
+
+    }
