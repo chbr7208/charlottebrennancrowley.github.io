37803e9d32ef19d59495690922c1471545b844f5
chbr7208 <chbr7208@colorado.edu>
2022-10-23 20:05:18 -0600

    changed jpg name to caps

--- js/assignment9.js
+++ js/assignment9.js
@@ -5,14 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const array = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const array = ['pic1.JPG', 'pic2.JPG', 'pic3.JPG', 'pic4.JPG', 'pic5.JPG'];
 
 /* Declaring the alternative text for each image file */
-const alt = {'pic1.jpg':'Michigan',
-'pic2.jpg':'Wisconsin', 
-'pic3.jpg':'Boulder Fall 1', 
-'pic4.jpg':'Boulder Fall 2', 
-'pic5.jpg':'Boulder Fall 3'};
+const alt = {'pic1.JPG':'Michigan',
+'pic2.JPG':'Wisconsin', 
+'pic3.JPG':'Boulder Fall 1', 
+'pic4.JPG':'Boulder Fall 2', 
+'pic5.JPG':'Boulder Fall 3'};
 
 /* Looping through images */
 let fullpath = "xxx";
