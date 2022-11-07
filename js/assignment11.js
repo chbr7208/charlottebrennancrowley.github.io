1b5b2331ce4f7d02ff7d14cef8f96c692790f3c5
chbr7208 <chbr7208@colorado.edu>
2022-11-06 17:38:23 -0700

    added a load bar to assignment 11

--- js/assignment11.js
+++ js/assignment11.js
@@ -20,4 +20,18 @@ function displayActivity(x){
     document.getElementById('js-activity-text').textContent = x;
 }
 
-getActivity();
+function update() {
+    var element = document.getElementById("myprogressBar");   
+    var width = 1;
+    var identity = setInterval(scene, 10);
+    function scene() {
+      if (width >= 100) {
+        clearInterval(identity);
+      } else {
+        width++; 
+        element.style.width = width + '%'; 
+      }
+    }
+  }
+
+  getActivity();
