a2a1fe2f0cb79cb1a6e80c07f9f5540519b18641
chbr7208 <chbr7208@colorado.edu>
2022-11-10 15:42:58 -0700

    started assignment 12 good luck to me

---
+++ js/assignment12.js
@@ -0,0 +1,21 @@
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const randomButton = document.querySelector(".random-button").addEventListener('click', random);
+const submitButton = document.querySelector(".submit-button").addEventListener('click', submit);
+
+
+function submit() {
+    alert(output.textContent);
+}
+
+function random(){
+    outputInt = randomNumber(0, 99999999999);
+    output.textContent = outputInt;
+}
+
+function randomNumber(min, max){
+    const num = Math.floor(Math.random() * ((max - min + 1)) + min);
+    return num;
+}
